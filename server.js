ee4ecc701c8091e1ee77752b55c310a9684db23b
PleatherShaman <shamanwebsolutions@gmail.com>
2019-09-08 19:36:33 +1000

    ready for first deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const app = express();
 const connectDB = require("./config/db");
 
@@ -7,6 +8,14 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use("/", require("./routes/api/dialogflow_routes.js"));
 app.use("/", require("./routes/api/fulfillment_routes.js"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
