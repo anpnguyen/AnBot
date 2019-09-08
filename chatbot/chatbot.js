8883e9dfcd5a0ba4ad1814d34859f105169fb904
PleatherShaman <shamanwebsolutions@gmail.com>
2019-09-08 19:48:19 +1000

    add JSON parse to goole private key

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -4,7 +4,7 @@ const Registration = require("../models/Registration");
 
 const credentials = {
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  private_key: process.env.GOOGLE_PRIVATE_KEY
+  private_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY)
 };
 const projectID = process.env.GOOGLE_PROJECT_ID;
 const sessionClient = new dialogflow.SessionsClient({
